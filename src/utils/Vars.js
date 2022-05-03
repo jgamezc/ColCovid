02eeb406058f3d213233b9b9a1d83b2beff29825
Parabel <jgamezc@uninorte.edu.co>
2022-05-03 03:06:22 -0500

    heroku url

--- src/utils/Vars.js
+++ src/utils/Vars.js
@@ -1,4 +1,4 @@
-const APIURL = "http://192.168.1.10:8080/"
+const APIURL = "https://colcovid-api.herokuapp.com/"
 const DATES = ["","2022-03-01","2022-01-01","2021-10-01","2021-04-01"]
 const TYPES = ["diagnosticos", "fallecidos"]
 
